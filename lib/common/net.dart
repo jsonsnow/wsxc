75aef226cc36af31620b4bac4675c053006bb432
jsonsnow <1183010734@qq.com>
2020-08-24 19:54:03 +0800

    feat<net>: net proxy config

--- lib/common/net.dart
+++ lib/common/net.dart
@@ -1,7 +1,9 @@
+import 'dart:io';
+import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:wsxc/common/global.dart';
-
+import 'package:wsxc/models/index.dart';
 
 class Net {
   Net([this.context]) {
@@ -10,9 +12,28 @@ class Net {
 
   BuildContext context;
   Options _options;
-  static Dio dio = Dio(BaseOptions(baseUrl: ''));
+  static Dio dio = Dio(BaseOptions(baseUrl: 'https://www.wegoab.com/'));
 
   static void init() {
     dio.options.headers["token"] = Global.profile.token;
+    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (client) {
+      client.findProxy = (uri) {
+        return 'PROXY 10.10.11.107:8888';
+      };
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+    };
+  }
+
+  Future<Map<String, dynamic>> home(
+      {String searchValue,
+      String searchImg,
+      String requestDataType,
+      num timestamp}) async {
+    print('start get home');
+    var r = await dio.get<Map<String, dynamic>>("album/moments");
+    print('home data: $r');
+    return r.data as Map<String, dynamic>;
   }
 }
--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,6 +1,7 @@
 import 'package:flukit/flukit.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:wsxc/common/index.dart';
 import 'package:wsxc/models/index.dart';
 import 'package:wsxc/widgets/home_widgets/home_cell.dart';
 
@@ -25,11 +26,17 @@ class _HomeRouterState extends State<HomeRouter> {
   }
 
   Widget _buildBody() {
+    fetchHomeData(false);
     return InfiniteListView<Goods>(
         onRetrieveData: (int page, List<Goods> items, bool refresh) async {
       return true;
     }, itemBuilder: (List list, int index, BuildContext ctx) {
       return HomeCell(list[index]);
     });
+  }
+
+  fetchHomeData(bool refresh) async {
+    var r = await Net(context).home();
+    var items = r['items'] as List<dynamic>;
   }
 }
